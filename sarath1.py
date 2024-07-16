3be54bed165b2a02da0e4dc5c9882c5f6a3267d1
unknown <dudisarath29@gmail.com>
2024-07-16 07:03:59 +0530

    return new code

---
+++ sarath1.py
@@ -0,0 +1,5 @@
+menu = ['idly','dosa','upma','pongal']
+
+for i in menu:
+     print(f"Today's menu in our hotel is {i.title()}")
+print("Thanks for visiting us,welcome again")
---
+++ sarath3.py
@@ -0,0 +1,7 @@
+age = input("enter the age:")
+
+if int(age) < 18:
+    print("You cant vote")
+name= input("Enter you name:")
+
+print(name)
